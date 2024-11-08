e8b975140454def3bcf138f4768ae102869f6c27
Edgar Robles <54951151+ed-robles@users.noreply.github.com>
2024-11-08 13:31:16 -0600

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -61,7 +61,7 @@ function Header() {
             <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs">
               {itemCount}
             </span>
-            <span>My Basket</span>
+            <span>Cart</span>
           </Link>
 
           {/* user area */}
@@ -72,7 +72,7 @@ function Header() {
                 className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-gray-900 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded"
               >
                 <PackageIcon className="w-6 h-6" />
-                <span>My Orders</span>
+                <span>Orders</span>
               </Link>
             )}
 
