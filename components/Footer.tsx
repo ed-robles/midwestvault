cc34e3326b3c9d7feff81bc83baf55cf5cca3d98
Edgar Robles <54951151+ed-robles@users.noreply.github.com>
2024-11-08 13:47:23 -0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className="bg-black w-full text-white">
+    <div className="bg-gray-900 w-full text-white">
       <div className="mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
             <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              MIDWEST<span className="italic">VAULT</span>
+              MIDWEST<span className="italic sans-serif">VAULT</span>
             </span>
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
@@ -24,7 +24,7 @@ export default function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center">
-          © 2024 <Link href="/" className="hover:underline">MIDWEST<span className="italic">VAULT</span></Link> - All Rights Reserved.
+          © 2024 <Link href="/" className="hover:underline">MIDWEST<span className="italic sans-serif">VAULT</span></Link> - All Rights Reserved.
         </span>
       </div>
     </div>
