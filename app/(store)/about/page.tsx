a37e5cf966efe6a400cfcba6ebf33dc04e9be902
Edgar Robles <robles_edgar@icloud.com>
2024-11-09 23:31:02 -0600

    Added pages

---
+++ app/(store)/about/page.tsx
@@ -0,0 +1,24 @@
+export default function AboutPage() {
+  return (
+    <>
+      <div className="w-full text-center">
+        <p className="text-lg my-12 p-12 bg-gray-900 text-white font-light">
+          Welcome to <span className="font-semibold">MIDWEST</span>
+          <span className="italic font-serif font-medium tracking-widest">
+            Vault
+          </span>{" "}
+          where quality meets exclusivity. Based in the heart of Wisconsin, we
+          are a curated, streetwear-inspired shop bringing you a selection of
+          clothing and accessories designed for those who value craftsmanship,
+          style, and individuality We believe in doing things differently.
+        </p>
+      </div>
+      <div className="w-full text-center">
+        <p className="text-lg my-12 p-12 text-gray-900 font-light">
+          Our collections are handpicked and available in limited quantities,
+          ensuring that each piece is as unique as the people who wear it.
+        </p>
+      </div>
+    </>
+  );
+}
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,30 +1,49 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="bg-gray-900 w-full text-white">
       <div className="mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <Link href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+          <Link
+            href="/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
             <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              MIDWEST<span className="italic font-serif font-medium tracking-widest">Vault</span>
+              MIDWEST
+              <span className="italic font-serif font-medium tracking-widest">
+                Vault
+              </span>
             </span>
           </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">About</a>
+              <a href="/about" className="hover:underline me-4 md:me-6">
+                About
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:underline me-4 md:me-6">Privacy Policy</a>
+              <a href="/privacy" className="hover:underline me-4 md:me-6">
+                Privacy Policy
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:underline">Contact</a>
+              <a href="#" className="hover:underline">
+                Contact
+              </a>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center">
-          © 2024 <Link href="/" className="hover:underline">MIDWEST<span className="italic font-serif font-medium tracking-widest">Vault</span></Link> - All Rights Reserved.
+          © 2024{" "}
+          <Link href="/" className="font-semibold hover:underline">
+            MIDWEST
+            <span className="italic font-serif font-medium tracking-widest">
+              Vault
+            </span>
+          </Link>{" "}
+          - All Rights Reserved.
         </span>
       </div>
     </div>
