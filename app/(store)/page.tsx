c5eefef19560af2d628adcb4b532c8adb3d7785a
Edgar Robles <robles_edgar@icloud.com>
2024-11-24 21:19:52 -0600

    UI updates

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -4,7 +4,7 @@ import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import { ChevronDown } from "lucide-react";
 
 export const dynamic = "force-static";
-export const revalidate = 60; // revalidare every 60 seconds
+export const revalidate = 60; // revalidate every 60 seconds
 
 export default async function Home() {
   const products = await getAllProducts();
@@ -16,12 +16,12 @@ export default async function Home() {
   );
 
   return (
-    <div className="font-mon">
+    <div className="flex flex-col min-h-screen font-mon">
       <div className="w-full font-bold text-gray-900 text-sm p-8 md:px-64 text-center">
         <p>DISCOVER A CURATED SELECTION OF QUALITY CLOTHING AND ACCESSORIES.</p>
         <ChevronDown className="animate-bounce w-6 h-6 mx-auto mt-6 -mb-4" />
       </div>
-      <div className="flex flex-col items-center justify-top min-h-screen bg-white p-4">
+      <div className="grow items-center justify-top p-4">
         <ProductsView products={products} categories={categories} />
       </div>
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,29 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="bg-gray-900 w-full text-white">
-      <div className="mx-auto p-4 md:py-8">
+      <div className="mx-auto p-3 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Link
             href="/"
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
+            {/* <span className="self-center text-2xl font-semibold whitespace-nowrap">
               MIDWEST
               <span className="italic font-serif font-medium tracking-widest">
                 Vault
               </span>
-            </span>
+            </span> */}
+            <Image
+              src="/MIDWESTVault_icon_white.png"
+              width={75}
+              height={75}
+              alt="MIDWEST Vault"
+            />
           </Link>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0">
+          <ul className="flex flex-wrap items-center mb-6 text-md font-medium sm:mb-0">
             <li>
               <a href="/about" className="hover:underline me-4 md:me-6">
                 About
@@ -37,12 +44,12 @@ export default function Footer() {
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center">
           © 2024{" "}
-          <Link href="/" className="font-semibold hover:underline">
+          {/* <Link href="/" className="font-semibold hover:underline">
             MIDWEST
             <span className="italic font-serif font-medium tracking-widest">
               Vault
             </span>
-          </Link>{" "}
+          </Link>{" "} */}
           - All Rights Reserved.
         </span>
       </div>
